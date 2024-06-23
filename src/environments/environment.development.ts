e9798b9fa55be13fffe207d40c1aefc891a0d806
noflexzoneee <142377196+noflexzoneee@users.noreply.github.com>
2024-06-22 23:51:41 -0500

    .

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -4,7 +4,7 @@ export const environment = {
   "avatar_decoration": "a_c6b3bc1dc49e5b284dca0b6437831004",
   "theme_colors": ['#0501FA', '#FA0101'],
   "profile_effect": {
-    "intro": "https://cdn.discordapp.com/assets/profile_effects/effects/2023-9-25/sayan/intro.png",
-    "loop": "https://cdn.discordapp.com/assets/profile_effects/effects/2023-9-25/sayan/loop.png",
+    "intro": "",
+    "loop": "https://cdn.discordapp.com/assets/profile_effects/effects/b17d139f2e9/fairy/loop.png",
   }
 };
